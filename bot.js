8cf98a9c4446b6d51de094ba444bc76e422ba32a
AShkurpelo <sania575@gmail.com>
2019-06-19 09:41:17 +0300

    Moved to Heroku

---
+++ bot.js
@@ -0,0 +1,32 @@
+const eway = require('./eway');
+const { url, token } = require('./config');
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(token);
+bot.setWebHook(url + token);
+
+bot.on('message', messageHandler);
+
+async function messageHandler(msg) {
+	let respMsg = 'Command format: <route number> <stop name>';
+	const parsed = parseMsg(msg);
+	if (parsed) {
+		const { routeNumber, stopName } = parsed;
+		respMsg = await eway.nextArrivalMsg(routeNumber, stopName);
+	}
+
+	bot.sendMessage(msg.chat.id, respMsg);
+}
+
+function parseMsg(msg) {
+	const words = msg.text.split(' ');
+	const routeNumber = words[0];
+	if (/\d+\S?/.test(routeNumber)) {
+		words = words.slice(1);
+		if (words.length && words[0].length) {
+			const stopName = words.join(' ');
+			return { routeNumber, stopName };
+		}
+	}
+	return null;
+}
