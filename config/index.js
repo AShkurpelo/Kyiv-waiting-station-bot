ce0ad912317f67e7ef14d15ff181fec033244c77
AShkurpelo <sania575@gmail.com>
2019-06-19 09:39:39 +0300

    Deleted port

--- config/index.js
+++ config/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 module.exports = {
-    port: process.env.PORT,
     url: process.env.BOT_API_URL,
     token: process.env.BOT_API_TOKEN,
     eway_api_routes: process.env.EWAY_API_ROUTES,
